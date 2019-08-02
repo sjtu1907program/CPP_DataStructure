0e7cd22f032206e6cb7b3a0516f902ce9aa122a8
Jay Hou <2558416766@qq.com>
2019-08-02 22:26:21 +0800

    Done
    Use the long long type to avoid truncation.

--- exercise1/Project1/main.cpp
+++ exercise1/Project1/main.cpp
@@ -1,8 +1,47 @@
 #include <iostream>
+#include <limits>
 
 using namespace std;
 
+int ReverseNumber(int n)
+{
+	long long result{};
+
+		while (n)
+		{
+			
+			if (numeric_limits<int>::max() - result*10 < n % 10 * 10 )
+			{
+				return numeric_limits<int>::max();
+			}
+			else
+			{
+				result *= 10;
+				result += n % 10;
+				n /= 10;
+			}
+		}
+
+	return result;
+}
+
+void test(int n) 
+{
+	auto result = ReverseNumber(n);
+	if (result == numeric_limits<int>::max())
+	{
+		cout << "Cannot reverse " << n << " overflow error " << endl;
+	}
+	else
+	{
+		cout << n << " -> " << result << endl;
+	}
+
+}
+
 void main() 
 {
-	cout << "Hello world!" << endl;
+	test(1234567899);
+	test(123);
+	test(-1234);	
 }
