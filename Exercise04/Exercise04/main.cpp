e41c9c8bc8e24b294713b44dd72bea5777c3ebdf
Milky.W <minniewu@qq.com>
2019-08-18 18:44:53 +0800

    add hw4

---
+++ Exercise04/Exercise04/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+#include <assert.h>
+
+using namespace std;
+
+string templateString(string input);
+string templateString(string input, string test);
+void test(string input, string result);
+
+int main() {
+	test("abc", "abc");
+	test("aaaa", "a");
+	test("abaaaabaaa", "abaaa");
+	test("abccabcc", "abcc");
+	test("abccd", "abccd");
+	return 0;
+}
+
+string templateString(string input)
+{
+	if (input == "")
+		return "";
+	string init;
+	init += input[0];
+	return templateString(input, init);
+	//return templateString(input, "" + input[0]);
+}
+
+string templateString(string input, string test)
+{
+	if (test.size() == input.size())
+		return input;
+
+	if (input.size() % test.size() != 0
+		|| input[test.size()] != test[0])
+		return templateString(input, test + input[test.size()]);
+
+	for (int i = 1; i < input.size(); i++) {
+		if (input[i] != test[i % test.size()])
+			return templateString(input, test + input[test.size()]);
+	}
+
+	return test;
+}
+
+void test(string input, string expected)
+{
+	cout << "input:" << input << endl;
+	string result = templateString(input);
+	cout << "result:" << result << endl;
+	cout << "expected:" << expected << endl << endl;
+	assert(result == expected);
+}
