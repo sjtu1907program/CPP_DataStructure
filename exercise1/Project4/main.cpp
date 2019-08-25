48970169eeadd07f8d4498c1b5ab8339ec5ffdff
Jay Hou <2558416766@qq.com>
2019-08-25 08:47:25 +0800

    SubString match

---
+++ exercise1/Project4/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string PatternMatch(string s)
+{
+	int n = s.size();
+	for (int i{ n / 2 }; i > 0; i--)
+	{
+		if (n % i == 0)
+		{
+			int c = n / i;
+			string t{};			
+			string res = s.substr(0, i);
+			for (int j = 0; j < c; ++j)
+			{
+				t += res;	
+			}
+			if (t == s)
+			{
+				return res;
+			}
+		}
+	}
+	return s;
+}
+
+void Test()
+{	//case senstive
+	string s1 = "eabceabc";//s1
+	string s2 = "aaaa";//a
+	string s3 = "abaaaabaaa";//abaaa
+	string s4 = "abccd";//abccd
+
+	cout << PatternMatch(s1) << endl;
+	cout << PatternMatch(s2) << endl;
+	cout << PatternMatch(s3) << endl;
+	cout << PatternMatch(s4) << endl;
+}
+
+int main()
+{
+	Test();
+	return 0;
+}
