fcaaa380f7681a0d00f373c5e60de112fc6508b2
Milky.W <minniewu@qq.com>
2019-08-25 15:13:01 +0800

    updated hw04

--- Exercise04/Exercise04/main.cpp
+++ Exercise04/Exercise04/main.cpp
@@ -4,9 +4,11 @@
 
 using namespace std;
 
-string templateString(string input);
-string templateString(string input, string test);
-void test(string input, string result);
+string templateString(const string& input);
+string templateString(const string& input, int length);
+int findNext(const string& input, int length);
+
+void test(const string& input, const string& result);
 
 int main() {
 	test("abc", "abc");
@@ -17,31 +19,43 @@ int main() {
 	return 0;
 }
 
-string templateString(string input)
+string templateString(const string& input)
 {
 	if (input == "")
 		return "";
-	return templateString(input, input.substr(0, 1));
+	return templateString(input, findNext(input, 1));
 }
 
-string templateString(string input, string test)
+string templateString(const string& input, int length)
 {
-	if (test.size() == input.size())
+	const int size = input.size();
+
+	if (length == size)
 		return input;
 
-	if (input.size() % test.size() != 0
-		|| input[test.size()] != test[0])
-		return templateString(input, test + input[test.size()]);
-
-	for (int i = 1; i < input.size(); i++) {
-		if (input[i] != test[i % test.size()])
-			return templateString(input, test + input[test.size()]);
+	for (int i = length + 1; i < size; i++) {
+		if (input[i] != input[i % length])
+			return templateString(input, findNext(input, length + 1));
 	}
 
-	return test;
+	return input.substr(0, length);
 }
 
-void test(string input, string expected)
+int findNext(const string& input, int length) {
+	const char first = input[0];
+	const int size = input.size();
+
+	while (length < input.length()) {
+		if (size % length == 0
+			&& input[length] == first) {
+			break;
+		}
+		length++;
+	}
+	return length;
+}
+
+void test(const string& input, const string& expected)
 {
 	cout << "input:" << input << endl;
 	string result = templateString(input);
