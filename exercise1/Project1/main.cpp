92ec71ebd41b2deba6036918a4a914a9c13f8e18
Decappoolla <823967701@qq.com>
2019-08-25 14:57:13 +0800

    Update main.cpp

--- exercise1/Project1/main.cpp
+++ exercise1/Project1/main.cpp
@@ -4,17 +4,17 @@ using namespace std;
 
 bool CheckPattern(string pat,string rest)//判断剩下的部分是否可以由pat循环组成
 {
-	if (pat == rest)return true;//递归终点
+	if (pat == rest)return true;//最后一段一样，递归终点
 	else if (pat == rest.substr(0, pat.length()))//比较第一段是否一样
 		return CheckPattern(pat, rest.substr(pat.length()));//递归比较剩下的一段是否由pat组成
-	else return false;//第一段即不一样，返回false
+	else return false;//第一段不一样，返回false
 }
 
 string FindPattern(string str)
 {
 	for (int i = 1; i < str.length() / 2; i++)
 	{
-		if (str[i] == str[0])
+		if (str[i] == str[0] && str.length()%i == 0)//与第一位一样，且此前字符数能被总长整除
 		{
 			if (CheckPattern(str.substr(0, i), str.substr(i)))
 				return str.substr(0, i);
